cec6cc32c1ed466f73bb2f742a441af3e33ebe69
cseltz <chase.seltz21@gmail.com>
2022-09-03 14:20:19 -0700

    Add comment total to post

--- client/src/components/single-post/PostComments.tsx
+++ client/src/components/single-post/PostComments.tsx
@@ -7,9 +7,18 @@ interface Props {
 }
 
 const PostComments = (props: Props) => {
+  // Total number of comments
+  const commentLength = props.comments.length;
+  let replyLength = 0;
+  props.comments.forEach((comment) => {
+    replyLength += comment.childComments.length;
+  });
+  const commentTotal = commentLength + replyLength;
+  //
+
   return (
     <div className="mt-8">
-      <h3 className="text-2xl text-opacity-90">Comments</h3>
+      <h3 className="text-2xl text-opacity-90">{commentTotal} Comments</h3>
       {props.comments.map((comment) => {
         return <PostComment key={comment._id} comment={comment} />;
       })}
