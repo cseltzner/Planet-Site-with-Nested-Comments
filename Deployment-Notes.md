42514ed7a877e8b8dbb6164e5e32c2acc06095ce
cseltz <chase.seltz21@gmail.com>
2022-09-14 19:16:26 -0700

    Minor changes to improve deployment

---
+++ Deployment-Notes.md
@@ -0,0 +1,11 @@
+## Connect to droplet:
+
+```bash
+ssh root@<ip>
+```
+
+## Transfer files to droplet
+
+```bash
+scp -r /path/to/my/files root@<ip> /path/on/my/server
+```
--- client/src/features/alert/alertActions.ts
+++ client/src/features/alert/alertActions.ts
@@ -1,11 +1,12 @@
 import { alertActions } from "./alertSlice";
 import { Dispatch } from "@reduxjs/toolkit";
 import { AlertTypes } from "../../util/alertTypes";
+import {v4 as uuidv4} from "uuid"
 
 export const setAlert =
   (msg: string, alertType: AlertTypes, timeout: number = 6000) =>
   (dispatch: Dispatch) => {
-    const id = crypto.randomUUID();
+    const id = uuidv4()
     dispatch(alertActions.setAlert({ msg, alertType, id }));
 
     setTimeout(() => {
